303db3635dabd6a0e1378bacd4d4062413edfa40
Zarfishan Ali <zf.ali@hotmail.com>
2025-02-11 13:45:49 +0000

    almost complete

--- caramelwebstudios/src/App.jsx
+++ caramelwebstudios/src/App.jsx
@@ -1,17 +1,20 @@
 ﻿import { useState } from 'react'
 import './App.css'
 import Home from './Home'
-
+import backgroundImage from "./assets/background.jpg";
 function App() {
 
   return (
-      <>
-
+      
+      <div className="full-page-bg" style={{ backgroundImage: `url(${backgroundImage})` }}>
           <Home />
-         
-         
+          <div className="row container">
+          <footer className="footer text-white">
+              <p>&copy; 2025 Caramel Web Studios. All Rights Reserved.</p>
+              </footer>
+          </div>
+    </div>
     
-    </>
   )
 }
 
--- caramelwebstudios/src/Home.jsx
+++ caramelwebstudios/src/Home.jsx
@@ -1,65 +1,63 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"; 
 
-const Home = () => {
-    const [loaded, setLoaded] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => setLoaded(true), 300); // Delay for smooth fade-in
-    }, []);
+function Home (){
+   
 
     return (
-        <div className="app">
-            {/* Background */}
-            <div className="background">
-                <div className={`overlay ${loaded ? "fade-in" : ""}`}>
-                    {/* Header */}
-                    
-                    <div className="container">
-                        <div >
-
-                            <a  href="#">
+      
+            <div className="container centered-box d-none d-md-flex shadow rounded-4 " style={{ width: "2000px", height: "300px" }}>
+               
+                    <div className="row justify-content-center">
+                        {/* Left Column */}
+                        <div className="col-3 d-flex flex-column justify-content-center align-items-center  text-white">
+                            <h4>Left Column</h4>
+                            <a href="#">
                                 <img src="logo.svg" alt="Caramel Web Studios" className="logo" />
                             </a>
-
                         </div>
-                            <div class='alert alert-primary' role='alert'>
-                  <p style={{ display: "none" }} className='d-block'>
-                      Bootstrap is now successfully installed 
-                            </p>
-                  <div>
-                            <ul>
-                                {["Home", "About", "Services", "Contact"].map((item, index) => (
-                                    <li className="nav-item" key={index}>
-                                        <a className="nav-link animated-link" href="#">
-                                            {item}
+
+                        {/* Right Column */}
+                        <div className="col-9 d-flex flex-column justify-content-center align-items-center">
+                            <h4>Right Column</h4>
+                            <div class='alert alert-primary ' role='alert'>
+                                <ul class="list-group list-group-flush">
+
+                                    <li className="nav-item">
+                                        <a className="nav-link animated-link" href="#"> Services
+
                                         </a>
                                     </li>
-                                ))}
+
+                                    <li className="nav-item">
+                                        <a className="nav-link animated-link" href="#"> Projects
+
+                                        </a>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <a className="nav-link animated-link" href="#"> Request A Quote
+
+                                        </a>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <a className="nav-link animated-link" href="#"> Contact
+
+                                        </a>
+                                    </li>
+
                                 </ul>
+
+
                             </div>
-              </div>
-          
                         </div>
+                    </div>
                 
-
-                   
-                </div>
-
-
-
-
-
-
             </div>
-            
-            <footer className="footer">
-                <p>&copy; 2025 Caramel Web Studios. All Rights Reserved.</p>
-            </footer>
-
-           
-          
-        </div>
+                    
+      
     );
 };
 
