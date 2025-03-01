efd6477d76ee40664da0cb8a0435238928b307f6
Zarfishan Ali <zf.ali@hotmail.com>
2025-03-01 20:59:34 +0000

    update

--- caramelwebstudios/vite.config.js
+++ caramelwebstudios/vite.config.js
@@ -4,6 +4,7 @@ import plugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
+    base: '/',
     server: {
         port: 50635,
     }
