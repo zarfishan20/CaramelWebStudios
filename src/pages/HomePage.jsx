8ae60f140982ac2a1cf9104a1bdfa0fba2b305db
Zarfishan Ali <zf.ali@hotmail.com>
2025-04-12 11:34:27 +0100

    minor changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import HeroSection from '../components/HeroSection'; // Import HeroSection
 import Projects from '../components/Projects';     // Import Projects
 import Services from '../components/Services';     // Import Services
@@ -12,19 +11,19 @@ const HomePage = () => {
       <HeroSection />
       
       {/* Services Section */}
-      <Container id="services" className="py-5">
+      
         <Services />
-      </Container>
+     
       
       {/* Projects Section */}
-      <Container id="projects" className="py-5">
+      
         <Projects />
-      </Container>
+    
       
       {/* Contact Section */}
-      <Container id="contact" className="py-5">
+
         <Contact />
-      </Container>
+   
     </>
   );
 };
