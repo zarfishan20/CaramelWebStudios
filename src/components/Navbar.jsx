efd7dd9e60efe5869813ea934455ccd8e5e84ca2
Zarfishan Ali <zf.ali@hotmail.com>
2025-04-14 10:43:14 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useRef, useEffect }  from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const CustomNavbar = () => (
-  <Navbar bg="light" expand="lg" className="shadow-sm p-3 fixed-top">
+const CustomNavbar = () => {
+
+
+  const [expanded, setExpanded] = useState(false);
+  const navRef = useRef(null);
+
+  // Close navbar on outside click
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (expanded && navRef.current && !navRef.current.contains(event.target)) {
+        setExpanded(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [expanded]);
+
+
+
+return(
+
+
+  <Navbar bg="" 
+  expand="lg" 
+  expanded={expanded}
+      onToggle={(value) => setExpanded(value)}
+  className="shadow-sm p-3 fixed-top"
+  ref={navRef}
+  >
     <Container>
       <Navbar.Brand as={Link} to="/" className="navbar-brand">Caramel Web Studios</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -18,6 +46,7 @@ const CustomNavbar = () => (
       </Navbar.Collapse>
     </Container>
   </Navbar>
-);
 
+);
+};
 export default CustomNavbar;
