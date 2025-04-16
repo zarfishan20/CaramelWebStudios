181f50c22fffc1c72c27e5b4b78f8eab44d35290
Zarfishan Ali <zf.ali@hotmail.com>
2025-04-16 22:11:44 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,10 @@ const Footer = () => (
   <footer className="text-center py-3 bg-light mt-4">
 
 <p>&copy; 2023 - {new Date().getFullYear()} Caramel Web Studios |
-   <a href="/privacy-policy">Privacy Policy</a> |
-    <a href="/terms-conditions">Terms & Conditions</a></p>
+   <p>
+  <Link to="/privacy-policy">Privacy Policy</Link> |{" "}
+  <Link to="/terms-conditions">Terms & Conditions</Link>
+</p>
 
   </footer>
 );
