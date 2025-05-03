adb11be21d1d9f249e44b0c4fff32bce5c12ae81
Zarfishan Ali <zf.ali@hotmail.com>
2025-05-03 11:13:49 +0100

    updatednotfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="notfound-container d-flex align-items-center justify-content-center">
-      <div className="notfound-card text-center">
-        <h1 className="notfound-heading">404</h1>
-        <p className="notfound-message">Oops! The page you're looking for doesn't exist.</p>
-        <Link to="/" className="btn notfound-btn">
-          Back to Home
-        </Link>
-      </div>
+    <div className="notfound-container">
+    <div className="planet planet-1"></div>
+    <div className="planet planet-2"></div>
+    <div className="planet planet-3"></div>
+
+    <div className="notfound-card text-center">
+      <h1 className="notfound-heading">404</h1>
+      <p className="notfound-message">Oops! You're lost in space.</p>
+      <Link to="/" className="btn notfound-btn">
+        Take Me Home
+      </Link>
     </div>
+  </div>
   );
 };
 
